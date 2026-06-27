a170279c2d1b5b4ea64fafbe44a539be01708de3
akash-senthil-06 <akashsenthil029@gmail.com>
2026-06-27 09:56:00 +0530

    [Module-07] Completed Spring Core Exercise 1 - Basic Spring Application

---
+++ 07-Spring-Core-Maven/Exercise 1 - Basic Spring Application/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,11 @@
+package com.library;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+public class LibraryManagementApplication {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = context.getBean("bookService", BookService.class);
+        bookService.registerBook("Spring Framework Fundamentals");
+        context.close();
+    }
+}
