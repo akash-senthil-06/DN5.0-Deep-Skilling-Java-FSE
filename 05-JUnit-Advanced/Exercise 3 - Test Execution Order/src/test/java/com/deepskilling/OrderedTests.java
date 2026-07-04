4bc5f34b5b1bc770322e4e713166766d8ee39feb
akash-senthil-06 <akashsenthil029@gmail.com>
2026-07-04 19:11:00 +0530

    Completed Module 05: JUnit Advanced Exercise 3 and added source code

--- 05-JUnit-Advanced/Exercise 3 - Test Execution Order/src/test/java/com/deepskilling/OrderedTests.java
+++ 05-JUnit-Advanced/Exercise 3 - Test Execution Order/src/test/java/com/deepskilling/OrderedTests.java
@@ -9,19 +9,19 @@ public class OrderedTests {
     @Test
     @Order(1)
     public void testFirst() {
-        System.out.println("    Executing first test (Order = 1)");
+        System.out.println("Executing first test (Order = 1)");
         assertTrue(true);
     }
     @Test
     @Order(2)
     public void testSecond() {
-        System.out.println("    Executing second test (Order = 2)");
+        System.out.println("Executing second test (Order = 2)");
         assertTrue(true);
     }
     @Test
     @Order(3)
     public void testThird() {
-        System.out.println("    Executing third test (Order = 3)");
+        System.out.println("Executing third test (Order = 3)");
         assertTrue(true);
     }
     public static void main(String[] args) {
